54a346efa7d01ee6b8262e2334d7590c341dce9b
Chandima Samaraweera <cssam1@hotmail.com>
2021-08-01 14:26:53 -0400

    Films implemented
    moved typesscript to dependencies

--- src/app/films/films.component.ts
+++ src/app/films/films.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import {Router} from '@angular/router';
+import {StartwarsApiService} from '../services/startwars-api.service';
+import {Film} from './Film';
+import {Person} from '../people/Person';
 
 @Component({
   selector: 'app-films',
@@ -7,9 +11,24 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FilmsComponent implements OnInit {
 
-  constructor() { }
+  public appError: string | undefined;
+  films: Film[] = [];
+  loading = true;
+  constructor(private router: Router, private startwarsApiService: StartwarsApiService) { }
 
   ngOnInit(): void {
+    this.getFilmList();
+    this.loading = false;
+  }
+
+  getFilmList(): any {
+    this.startwarsApiService.getFilms().subscribe((res) => {
+        if (res.status === 200){
+          this.films = (res.body.results as [Film]);
+        }
+      },
+      error => this.appError = error);
+
   }
 
 }
--- src/app/services/startwars-api.service.ts
+++ src/app/services/startwars-api.service.ts
@@ -24,6 +24,47 @@ export class StartwarsApiService extends ApiService {
       );
   }
 
+  getFilms(): Observable<HttpResponse<any>>{
+    const endpoint = this.starwarsBaseUrl + 'films/';
+    return this.http.get<HttpResponse<any>>(endpoint, { observe: 'response' })
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  getPlanets(page: number): Observable<HttpResponse<any>>{
+    const endpoint = this.starwarsBaseUrl + 'planets/?page=' + page;
+    return this.http.get<HttpResponse<any>>(endpoint, { observe: 'response' })
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  getStarships(page: number): Observable<HttpResponse<any>>{
+    const endpoint = this.starwarsBaseUrl + 'starships/?page=' + page;
+    return this.http.get<HttpResponse<any>>(endpoint, { observe: 'response' })
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  getVehicles(page: number): Observable<HttpResponse<any>>{
+    const endpoint = this.starwarsBaseUrl + 'vehicles/?page=' + page;
+    return this.http.get<HttpResponse<any>>(endpoint, { observe: 'response' })
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+  getSpecies(page: number): Observable<HttpResponse<any>>{
+    const endpoint = this.starwarsBaseUrl + 'species/?page=' + page;
+    return this.http.get<HttpResponse<any>>(endpoint, { observe: 'response' })
+      .pipe(
+        catchError(this.handleError)
+      );
+  }
+
+
   getFromDirectEndpoint(specieUrl: string): Observable<HttpResponse<any>> {
     return this.http.get<HttpResponse<any>>(specieUrl, { observe: 'response' })
       .pipe(
