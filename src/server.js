425de3144df28e32db623ccef273d3152b10d091
Chandima Samaraweera <cssam1@hotmail.com>
2021-08-01 23:29:01 -0400

    added path and static assets in server.js to see to show logo postman  testing added for further references

--- src/server.js
+++ src/server.js
@@ -7,10 +7,12 @@ function requireHTTPS(req, res, next) {
 }
 
 const express = require('express');
+const path = require('path');
 const app = express();
 
 app.use(requireHTTPS);
 app.use(express.static('./dist/StarwarsClient'));
+app.use(express.static(path.join(__dirname, "assets")));
 
 app.get('/*', (req, res) =>
   res.sendFile('index.html', {root: 'dist/StarwarsClient/'}),
